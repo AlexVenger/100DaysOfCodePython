563548b6b5cc0476db337104b6738292c980d71e
Igor <alexvenger456@gmail.com>
2023-07-26 17:28:09 +0400

    Stonks: step 1 done

--- 36.Stonks/stonks.py
+++ 36.Stonks/stonks.py
@@ -1,5 +1,13 @@
+import requests
+import json
+import datetime
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+
+with open("keys.json") as keys_file:
+	keys = json.load(keys_file)
+	alpha_vantage_key = keys["alpha_vantage_key"]
 
 # STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -21,3 +29,26 @@ or
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
+
+alpha_vantage_url = \
+	'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
+alpha_vantage_params = {
+	"function": "TIME_SERIES_DAILY",
+	"symbol": STOCK,
+	"apikey": alpha_vantage_key
+}
+r = requests.get(url=alpha_vantage_url, params=alpha_vantage_params)
+data = r.json()
+time_series = data["Time Series (Daily)"]
+yesterday = datetime.date.today() - datetime.timedelta(days=1)
+day_before_yesterday = yesterday - datetime.timedelta(days=1)
+
+prev_stonk = time_series[str(day_before_yesterday)]
+stonk = time_series[str(yesterday)]
+delta = float(stonk["1. open"]) * 100 / float(prev_stonk["1. open"])
+if delta > 105 or delta < 95:
+	print("Get News")
+
+
+with open("stonks.json", "w") as stonk_file:
+	json.dump(data, stonk_file, indent=2)
