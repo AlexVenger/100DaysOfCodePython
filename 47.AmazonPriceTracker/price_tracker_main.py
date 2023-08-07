1a5096165ff1c29f44126190c9388da8c1b57adc
AlexVenger <alexvenger456@gmail.com>
2023-08-07 18:53:14 +0400

    Amazon Price Tracker: done

--- 47.AmazonPriceTracker/price_tracker_main.py
+++ 47.AmazonPriceTracker/price_tracker_main.py
@@ -1,16 +1,30 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import smtplib
+import lxml
 
 with open("product_data.json") as product_data:
 	data = json.load(product_data)
 	url = data["url"]
 	headers = data["headers"]
+	email = data["email"]
+	password = data["password"]
+	to = data["to"]
 
 response = requests.get(url=url, headers=headers)
 
-soup = BeautifulSoup(response.text, "html.parser")
-print(soup)
+soup = BeautifulSoup(response.text, "lxml")
 
-price = soup.select_one("#corePrice_feature_div span.a-offscreen")
-print(price.text)
+price_element = soup.select_one("span.a-offscreen")
+price = float(price_element.text.replace("$", ""))
+
+if price < 35:
+	with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+		connection.starttls()
+		connection.login(user=email, password=password)
+		connection.sendmail(
+			from_addr=email,
+			to_addrs=to,
+			msg=f"Subject: Amazon Price Drop Alert\n\nThe price of a product you wanted has dropped!\nCheck here: {url}"
+		)
