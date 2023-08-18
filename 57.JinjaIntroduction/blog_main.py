43ca4a713db4486000cef5afe05e04d9dce42d50
Igor <alexvenger456@gmail.com>
2023-08-18 17:26:35 +0400

    Jinja Introduction: done

---
+++ 57.JinjaIntroduction/blog_main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import requests
+
+
+app = Flask(__name__)
+
+blogs_url = "https://api.npoint.io/c790b4d5cab58020d391"
+blogs = requests.get(blogs_url)
+posts = blogs.json()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", posts=posts)
+
+
+@app.route("/post/<post_id>")
+def post_page(post_id):
+    for post in posts:
+        if post["id"] == int(post_id):
+            return render_template("post.html", post=post)
+    return None
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- 57.JinjaIntroduction/jinja_basic.py
+++ 57.JinjaIntroduction/jinja_basic.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import random
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -11,7 +12,17 @@ YEAR = datetime.now().year
 @app.route("/")
 def home():
 	random_number = random.random()
-	return render_template("index.html", num=random_number, name=MY_NAME, year=YEAR)
+	return render_template("homepage.html", num=random_number, name=MY_NAME, year=YEAR)
+
+
+@app.route("/guess/<name>")
+def guesser(name: str):
+	params = {"name": name}
+	age_data = requests.get(url="https://api.agify.io/", params=params)
+	age = age_data.json()["age"]
+	gender_data = requests.get(url="https://api.genderize.io/", params=params)
+	gender = gender_data.json()["gender"]
+	return render_template("guess.html", name=name.capitalize(), age=age, gender=gender)
 
 
 if __name__ == "__main__":
