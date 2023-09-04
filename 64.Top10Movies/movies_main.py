821b88fb048fd5c70c2ad2216f7d2dd6fe08025f
AlexVenger <alexvenger456@gmail.com>
2023-09-04 14:32:15 +0400

    Top 10 Movies: the beginning

---
+++ 64.Top10Movies/movies_main.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movie-collection.db"
+db.init_app(app)
+Bootstrap5(app)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String, nullable=False)
+    img_url = db.Column(db.String, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    # second_movie = Movie(
+    #     title="Avatar The Way of Water",
+    #     year=2022,
+    #     description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+    #     rating=7.3,
+    #     ranking=9,
+    #     review="I liked the water.",
+    #     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+    # )
+    # db.session.add(second_movie)
+    # db.session.commit()
+    movies = db.session.execute(db.select(Movie).order_by(Movie.id)).scalars()
+    return render_template("index.html", movies=movies)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
